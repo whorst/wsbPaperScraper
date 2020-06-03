b12038db94829de3a669d53237e87fadcd110f73
William Horst <you@example.com>
2020-06-03 17:36:42 -0400

    Add words to ignore

--- redditScraper.py
+++ redditScraper.py
@@ -24,17 +24,22 @@ def getTickerInComment(comment):
     return re.findall(r'([$]?[A-Z]?[A-Z]?[A-Z]{1}[A-Z]{1})\s', comment.body)
 
 def getMostCommonTickers():
-    return ["MSFT", "BA", "SPY", "BABA", "SQQQ", "SPXS", "TSLA", "DIS", "AMZN", "AMD", "LYFT", "NDAQ", "QQQ", "SPCE", "FB", "DKNG", "ZM", "VXX"]
+    return ["MSFT", "BA", "SPY", "BABA", "SQQQ", "SPXS", "TSLA", "DIS", "AMZN", "AMD", "LYFT", "NDAQ", "QQQ", "SPCE",
+            "FB", "DKNG", "ZM", "VXX", "WORK", "LULU", "MU", "DOQ", "DIS", "DAL", "HOG", "SNAP"]
 
 def getExclusionWord():
-    return ["ALL", "US", "USSR", "ITM", "AND", "OTM", "USD", "CAD", "PE", "I", "SAAS", "GTFO", "BTFD", "DD", "LMAO", "LOL", "AMA"]
+    return ["ALL", "US", "USSR", "ITM", "AND", "RIP" "OTM", "USD","EOD", "CAD", "PE","YOLO", "I", "SAAS", "GIGS", "GTFO", "BTFD", "EXP","MINS", "PP", "DD", "LMAO", "LOL", "AMA", "TLDR", "RN", "TME", "GUH", "FUK", "WUT", "WAT","WSB", "TEH", "WTF", "FOMO","IDK", "IMO", "PLS"]
 
 def validateDatePriceAndTickerInComment(comment):
     occurencesOfStrikeDate = getStrikeDatesInComment(comment)
     occurencesOfPrice = getPriceInComment(comment)
     occurencesOfTicker = getTickerInComment(comment)
-    if (bool(occurencesOfPrice) and bool(occurencesOfStrikeDate) ):
+    # if (bool(occurencesOfPrice) and bool(occurencesOfStrikeDate) ):
+    if (bool(occurencesOfStrikeDate) ):
         print(comment.body + "\n")
+        file = open("commentFile", "a")
+        file.write(comment.body + "\n\n")
+        file.close()
 
 
 def printValidComments(submission_id):
@@ -44,10 +49,6 @@ def printValidComments(submission_id):
             if(commentSubmissionId == submission_id):
                 if("http" not in comment.body):
                     validateDatePriceAndTickerInComment(comment)
-                    # print(comment.body + "\n")
-                    # file = open("commentFile", "a")
-                    # file.write(comment.body + "\n\n")
-                    # file.close()
         except UnicodeEncodeError:
             pass
 
@@ -57,6 +58,40 @@ for submission in reddit.subreddit("wallstreetbets").hot(limit=1):
         printValidComments(submission.id)
     if ("What Are Your Moves Tomorrow" in submission.title):
         printValidComments(submission.id)
+
+# Traceback (most recent call last):
+#   File "C:/Users/William/PycharmProjects/redditCommentScraper/redditScraper.py", line 58, in <module>
+#     printValidComments(submission.id)
+#   File "C:/Users/William/PycharmProjects/redditCommentScraper/redditScraper.py", line 46, in printValidComments
+#     for comment in reddit.subreddit("wallstreetbets").stream.comments():
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\models\util.py", line 186, in stream_generator
+#     for item in reversed(list(function(limit=limit, **function_kwargs))):
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\models\listing\generator.py", line 61, in __next__
+#     self._next_batch()
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\models\listing\generator.py", line 71, in _next_batch
+#     self._listing = self._reddit.get(self.url, params=self.params)
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\reddit.py", line 490, in get
+#     return self._objectify_request(method="GET", params=params, path=path)
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\reddit.py", line 573, in _objectify_request
+#     self.request(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\praw\reddit.py", line 726, in request
+#     return self._core.request(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 332, in request
+#     return self._request_with_retries(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 252, in _request_with_retries
+#     return self._do_retry(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 162, in _do_retry
+#     return self._request_with_retries(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 252, in _request_with_retries
+#     return self._do_retry(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 162, in _do_retry
+#     return self._request_with_retries(
+#   File "C:\Users\William\PycharmProjects\redditCommentScraper\venv\lib\site-packages\prawcore\sessions.py", line 265, in _request_with_retries
+#     raise self.STATUS_EXCEPTIONS[response.status_code](response)
+# prawcore.exceptions.ServerError: received 503 HTTP response
+
+
+
 
 
 # Traceback (most recent call last):
