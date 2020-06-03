6723131010df997928ee8d879ccf5bb0be34fded
whorst042 <whorst042@gmail.com>
2020-06-03 00:30:03 -0400

    Second Commit

--- redditScraper.py
+++ redditScraper.py
@@ -1,7 +1,10 @@
+import re
 from idlelib.multicall import r
 
 import praw
 from datetime import date
+
+# Valid regex to get date with p or c appended
 
 reddit = praw.Reddit('bot1')
 
@@ -11,25 +14,49 @@ reddit = praw.Reddit('bot1')
 # print(subreddit.hot())
 # exit(0)
 
-def getContinuousRedditStream(submission, submission_id):
+def getStrikeDatesInComment(comment):
+    return re.findall(r'\s([0-9]{0,1}[0-9]{1}/[0-9]{1}[0-9]{0,1}[(p|c)]?)\s', comment.body)
+
+def getPriceInComment(comment):
+    return re.findall(r'\s([$]?[0-9]?[0-9]?[0-9]?[0-9]+[(p|c)]?)\s', comment.body)
+
+def getTickerInComment(comment):
+    return re.findall(r'([$]?[A-Z]?[A-Z]?[A-Z]{1}[A-Z]{1})\s', comment.body)
+
+def getMostCommonTickers():
+    return ["MSFT", "BA", "SPY", "BABA", "SQQQ", "SPXS", "TSLA", "DIS", "AMZN", "AMD", "LYFT", "NDAQ", "QQQ", "SPCE", "FB", "DKNG", "ZM", "VXX"]
+
+def getExclusionWord():
+    return ["ALL", "US", "USSR", "ITM", "AND", "OTM", "USD", "CAD", "PE", "I", "SAAS", "GTFO", "BTFD", "DD", "LMAO", "LOL", "AMA"]
+
+def validateDatePriceAndTickerInComment(comment):
+    occurencesOfStrikeDate = getStrikeDatesInComment(comment)
+    occurencesOfPrice = getPriceInComment(comment)
+    occurencesOfTicker = getTickerInComment(comment)
+    if (bool(occurencesOfPrice) and bool(occurencesOfStrikeDate) ):
+        print(comment.body + "\n")
+
+
+def printValidComments(submission_id):
     for comment in reddit.subreddit("wallstreetbets").stream.comments():
         try:
             commentSubmissionId = comment.link_id[-6:]
             if(commentSubmissionId == submission_id):
-                if("/" in comment.body):
-                    print(comment.body + "\n")
-                    file = open("commentFile", "a")
-                    file.write(comment.body + "\n\n")
-                    file.close()
+                if("http" not in comment.body):
+                    validateDatePriceAndTickerInComment(comment)
+                    # print(comment.body + "\n")
+                    # file = open("commentFile", "a")
+                    # file.write(comment.body + "\n\n")
+                    # file.close()
         except UnicodeEncodeError:
             pass
 
 for submission in reddit.subreddit("wallstreetbets").hot(limit=1):
     print(submission.title)
     if ("Daily Discussion Thread for" in  submission.title):
-        getContinuousRedditStream(submission, submission.id)
+        printValidComments(submission.id)
     if ("What Are Your Moves Tomorrow" in submission.title):
-        getContinuousRedditStream(submission, submission.id)
+        printValidComments(submission.id)
 
 
 # Traceback (most recent call last):
