fc2704b788c218a909766250c293a662cb920912
William Horst <you@example.com>
2020-06-28 16:46:58 -0400

    change keys

--- paperTrading/paperTradingUtilities.py
+++ paperTrading/paperTradingUtilities.py
@@ -63,16 +63,16 @@ def openInversePositions(api, newId, positionObject):
 
 def getRestApiInterface():
     #authentication and connection details
-    api_key = 'PK22MZ9LJ5G2NA4UCPHI'
-    api_secret = 'yRa4pT3tECXJ4ij88SnQVJJeu972exmxtqg8lxXR'
+    api_key = 'PK88YTDVNV64L62GF2DO'
+    api_secret = '/5v3oTnWpQv89BjTUoCcEG1VAkVUxSZbW/MNFCAF'
     base_url = 'https://paper-api.alpaca.markets'
     #instantiate REST API
     return tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
 
 def getRestApiInterfaceInverse():
     ##Inverse
-    api_key = 'PK4P8ZBJ35OCTOLNYHMA'
-    api_secret = 'SSs3404C9EvXjl3RwtjOnMA2WmDn/kof9tMljBSk'
+    api_key = 'PKLW4891U0DACFSEZ41B'
+    api_secret = 'Jxw5u8qcQW9J6V9BPXj/YkMIgktG8bcfzLkvgB2h'
     base_url = 'https://paper-api.alpaca.markets'
     #instantiate REST API
     return tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
